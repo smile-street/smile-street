ab9820f73e9d94dbc19ddc382e50335c6afc9b9a
Ariel Pillemer <pillemer@gmail.com>
2021-02-18 17:39:23 +0000

    play with divs to try and sort out the spacing of the buttons

--- smile-street-react/src/components/Login/Login.js
+++ smile-street-react/src/components/Login/Login.js
@@ -1,73 +1,59 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Link from '@material-ui/core/Link';
+import TextField from "@material-ui/core/TextField";
 import "./Login.css";
 
 const Login = () => {
-    return ( 
-        <Paper>
-		<div className="mui-panel">
+	return (
+		<Container maxWidth="sm">
 			<img src="../images/SmileStreetLogo.png" alt="logo" width="100%" />
-			
-			<form className="mui-form">
-				<legend>Login</legend>
-				<TextField required id="filled-basic" variant="filled" label="Email"/>
+			<Paper height={100}>
+				<div>
+					<legend>Login</legend>
+					<TextField required id="filled-basic" variant="filled" label="Email" />
 
-				<TextField
-					required
-					id="filled-password-input"
-					label="Password"
-					type="password"
-					variant="filled"
-				/>
+					<TextField
+						required
+						id="filled-password-input"
+						label="Password"
+						type="password"
+						variant="filled"
+					/>
+				</div>
+					<Button className="button-colour" variant="contained">
+						Login
+					</Button>
+				<div className="mui-col-md-12 mui-col-xs-12">
+					<label>Or</label>
+				</div>
+				<div className="mui-col-md-12 mui-col-xs-12">
+					<Link  href="#">Forgot your password click here!</Link>
+				</div>
+				<div>
+					<Button className="button-colour" variant="contained">
+						Login with Google
+        			</Button>
+					<Button className="button-colour" variant="contained">
+						Login with Facebook
+        			</Button>
+				</div>
+			</Paper>
 
-				<div className="mui-row">
-                    <Button className="button-colour" variant="contained">
-                        Login
-                    </Button>
-				</div>
-
-				<div className="mui-row">
-					<div className="mui-col-md-12 mui-col-xs-12">
-						<label>Or</label>
-					</div>
-				</div>
-				<div className="mui-row">
-					<div className="mui-col-md-12 mui-col-xs-12">
-						<a>Forgot your password click here!</a>
-					</div>
-				</div>
-				<div className="mui-row">
-				<div className="mui-row">
-                    <Button className="button-colour" variant="contained">
-                        Login with Google
-                    </Button>
-				</div>
-				<div className="mui-row">
-                    <Button className="button-colour" variant="contained">
-                        Login with Facebook
-                    </Button>
-				</div>
-				</div>
-				<div className="mui-divider"></div>
-				<div className="mui-row">
-				<div className="mui-row">
-                    <Button className="button-colour" variant="contained">
-                        Signup as a Volunteer
-                    </Button>
-				</div>
-				<div className="mui-row">
-                    <Button className="button-colour" variant="contained">
-                        signup as a Good Cause
-                    </Button>
-				</div>
-				</div>
-			</form>
-
-		</div>
-	</Paper>
+			<div>
+				<Paper>
+					<Button className="button-colour" variant="contained">
+						Sign up as a Volunteer
+        			</Button>
+					<Button className="button-colour" variant="contained">
+						Sign up as a Good Cause
+					</Button>
+				</Paper>
+			</div>
+		</Container>
 	);
-}
+};
 
 export default Login;
