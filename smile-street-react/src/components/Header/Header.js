373936eeaf8d210227b4a6884a725e8d9881cd01
taslima26 <banglawalas@hotmail.co.uk>
2021-02-18 18:53:45 +0000

    tidy up header file layout cod

--- smile-street-react/src/components/Header/Header.js
+++ smile-street-react/src/components/Header/Header.js
@@ -5,34 +5,20 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
 const Header = () => {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: "center",
-      color: theme.palette.text.secondary,
-    },
-  }));
-
-  const classes = useStyles();
   return (
     <>
       <Container>
-        <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={12}>
-              <Paper className={classes.paper}>
-                <img
-                  src="../images/SmileStreetLogo.png"
-                  alt="logo"
-                  width="100%"
-                />
-              </Paper>
-            </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12}>
+            <Paper>
+              <img
+                src="../images/SmileStreetLogo.png"
+                alt="logo"
+                width="100%"
+              />
+            </Paper>
           </Grid>
-        </div>
+        </Grid>
       </Container>
     </>
   );
