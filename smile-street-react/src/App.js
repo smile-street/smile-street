917f05c81842b688e817074dd01288fc9cae49bc
Pushpa2509 <pushpar.blr@gmail.com>
2021-02-19 18:01:22 +0000

    volunteer matches yes no button

--- smile-street-react/src/App.js
+++ smile-street-react/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Login from './components/Login/Login'
+//import Login from './components/Login/Login'
+import VolunteermatchesYesNoButton from './components/Volunteermatches/VolunteermatchesYes-No-Button'
 
 function App() {
 	return (
 
 		<div className="App">
 
-			<Login />
+			<VolunteermatchesYesNoButton />
 
 		</div>
 	);
---
+++ smile-street-react/src/components/Volunteermatches/VolunteermatchesYes-No-Button.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Container from 'muicss/lib/react/container';
+//import Row from 'muicss/lib/react/row';
+//import Col from 'muicss/lib/react/col';
+import Button from '@material-ui/core/Button';
+import "./VolunteermatchesYes-No-Button.css";
+//import Option from 'muicss/lib/react/option';
+//import Select from 'muicss/lib/react/select';
+
+const VolunteermatchesYesNoButton = () => {
+    return (
+        <div className="mui-container mui--text-center">
+		<div className="mui-panel">
+			<img src="../images/SmileStreetLogo.png" alt="logo image" width="100%"/>
+			<legend className="mui--text-center"> My Matches</legend>
+			<div>
+				<button className="mui-btn mui--text-right">Edit</button>
+				<div>
+					<div className="mui-panel">
+						<div className="mui--text-left mui-col-xs-6 mui-col-6">
+							<p>Good cause name:</p>
+							<p>Location:</p>
+							<p>Dates:</p>
+							<p>Description:</p>
+						</div>
+						<button class="mui--divider-right mui-btn mui-btn--large mui-btn--raised">Yes, Please</button>
+						<button class="mui--divider-right mui-btn mui-btn--large mui-btn--raised">No, Thanks</button>
+					</div>
+					<div className="mui-panel">
+						<div class="mui--text-left mui-col-xs-6 mui-col-6">
+							<p>Good cause name:</p>
+							<p>Location:</p>
+							<p>Dates:</p>
+							<p>Description:</p>
+						</div>
+						<button className="mui--divider-right mui-btn mui-btn--large mui-btn--raised">Yes, Please</button>
+						<button className="mui-btn mui-btn--large mui-btn--raised">No, Thanks</button>
+					</div>
+					<div className="mui-panel">
+						<div className="mui--text-left mui-col-xs-6 mui-col-6">
+							<p>Good cause name:</p>
+							<p>Location:</p>
+							<p>Dates:</p>
+							<p>Description:</p>
+						</div>
+						<button className="mui-btn mui-btn--large mui-btn--raised">Yes, Please</button>
+						<button className="mui-btn mui-btn--large mui-btn--raised">No, Thanks</button>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+	);
+}
+export default VolunteermatchesYesNoButton;
