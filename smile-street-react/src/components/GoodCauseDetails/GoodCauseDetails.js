eef370fdb953169e0567b36415657dbea4d8428a
shinesha <jodrew_7@hotmail.com>
2021-02-20 16:58:00 +0000

    good_cause_details.js

---
+++ smile-street-react/src/components/GoodCauseDetails/GoodCauseDetails.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { TextField, Button } from "@material-ui/core";
+
+function GoodCauseDetails() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      width: "100%",
+      alignContent: "center",
+    },
+    buttonColor: {
+      backgroundColor: "#53bd98",
+      color: "white",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+          <Container className={classes.root}>
+
+          <Paper className={classes.paper}>
+          <Grid container spacing={3}>
+
+            <Grid item xs={12} sm={6}>
+                <TextField
+                  id="standard-full-width"
+                  style={{ margin: 8 }}
+                  placeholder="charity registration number"
+                  fullWidth
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                  />
+            </Grid>
+
+
+
+
+            <Grid item xs={12} sm={6}>
+                <TextField
+                  id="standard-full-width"
+                  style={{ margin: 8 }}
+                  label = "charity registered name"
+                  fullWidth
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true,
+                    readOnly: true
+                  }}
+                  variant="outlined"
+                  />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                  id="standard-full-width"
+                  style={{ margin: 8 }}
+                  placeholder="Please describe your organisation & charitable aims"
+                  fullWidth
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                  multiline
+                  rowsMax={25}
+                  />
+            </Grid>
+
+
+
+
+            <Grid item xs={12} sm={12}>
+              <Button variant="contained" className={classes.buttonColor}>
+                Add Volunteering Oportunities
+              </Button>
+            </Grid>
+          </Grid>
+            </Paper>
+
+    </Container>
+    </div>
+
+  );
+}
+
+export default GoodCauseDetails;
