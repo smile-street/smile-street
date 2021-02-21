51060916ae6a060b4949e87d969d363f92f2c16e
Ariel Pillemer <pillemer@gmail.com>
2021-02-21 14:33:10 +0000

    copy template from material-ui website

--- smile-street-react/src/components/Login/Login.js
+++ smile-street-react/src/components/Login/Login.js
@@ -3,141 +3,135 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography, Grid } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
+    marginTop: theme.spacing(0.5),
     padding: theme.spacing(3),
     textAlign: "center",
     color: theme.palette.text.secondary,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  buttonColor: {
+  button: {
     backgroundColor: "#53bd98",
     color: "white",
     "&:hover": {
       background: "#449f80",
     },
+    margin: theme.spacing(1, 0, 0),
   },
-  justifyButton: {
-    alignContent: "center",
+  form: {
+    width: '100%',
   },
 }));
 
 const Login = () => {
   const classes = useStyles();
   return (
-    <Container>
+    <Container component="main" maxWidth="md">
+      <Paper className={classes.paper}>
+        <Typography component="h1" variant="h5">
+            Sign in
+        </Typography>
 
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={3} className={classes.justifyButton}>
-              <Paper className={classes.paper} justifyContent="center">
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  justifyContent="center"
-                  className={classes.buttonColor}
-                >
-                  Log in
-                </Button>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <Paper className={classes.paper}>
-                <Link href="#" variant="body2">
-                  Forgot password? Click here!
-                </Link>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.buttonColor}
-                >
-                  Log in with Facebook
-                </Button>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.buttonColor}
-                >
-                  Log in with Google
-                </Button>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.buttonColor}
-                >
-                  Register as a Good Cause
-                </Button>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.buttonColor}
-                >
-                  Register as a Volunteer
-                </Button>
-              </Paper>
-            </Grid>
-          </Grid>
+      <form className={classes.form}>
+    <Grid container>
+      <Grid item >
+
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+      </Grid>      
+      <Grid item >
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+
+      </Grid>
+    </Grid>
+
+
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Log in
+        </Button>
+
+        <Link href="#" variant="body2">
+          Forgot password? Click here!
+        </Link>
+      </form>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Log in with Facebook
+            </Button>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Log in with Google
+            </Button>
+      </Paper>
+
+      <Paper className={classes.paper}>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Register as a Good Cause
+        </Button>
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Register as a Volunteer
+        </Button>
+      </Paper>
+
     </Container>
   );
 };
