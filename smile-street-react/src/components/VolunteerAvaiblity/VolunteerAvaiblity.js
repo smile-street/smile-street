21c3c165761dfa6af7154d416b634cd7cdcbfeda
taslima26 <banglawalas@hotmail.co.uk>
2021-02-22 17:44:58 +0000

    grid is nice and central now

--- smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvaiblity.js
+++ smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvaiblity.js
@@ -32,100 +32,109 @@ const VolunteerAvaiblity = () => {
     <Container>
       <form>
         <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              {/* <Paper className={classes.paper}> */}
-              <FormControl className={classes.formElementSize}>
-                <TextField
-                  id="outlined-basic"
-                  label="Employer Name"
-                  variant="outlined"
-                />
-              </FormControl>
-              {/* </Paper> */}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              {/* <Paper className={classes.paper}> */}
-              <FormControl className={classes.formElementSize} variant="filled">
-                <InputLabel id="demo-simple-select-filled-label" sm={6}>
-                  Select your primary match Location
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-filled-label"
-                  id="demo-simple-select-filled"
+          <Paper className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                {/* <Paper className={classes.paper}> */}
+                <FormControl className={classes.formElementSize}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Employer Name"
+                    variant="outlined"
+                  />
+                </FormControl>
+                {/* </Paper> */}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                {/* <Paper className={classes.paper}> */}
+                <FormControl
+                  className={classes.formElementSize}
+                  variant="filled"
                 >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              {/* </Paper> */}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              {/* <Paper className={classes.paper}> */}
-              <FormControl
-                style={{ minWidth: 300 }}
-                variant="filled"
-                className={classes.formControl}
-              >
-                <InputLabel id="demo-simple-select-filled-label" sm={6}>
-                  Select your secondary match Location
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-filled-label"
-                  id="demo-simple-select-filled"
+                  <InputLabel id="demo-simple-select-filled-label" sm={6}>
+                    Select your primary match Location
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-filled-label"
+                    id="demo-simple-select-filled"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+                {/* </Paper> */}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                {/* <Paper className={classes.paper}> */}
+                <FormControl
+                  style={{ minWidth: 300 }}
+                  variant="filled"
+                  className={classes.formControl}
                 >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              {/* </Paper> */}
-            </Grid>
+                  <InputLabel id="demo-simple-select-filled-label" sm={6}>
+                    Select your secondary match Location
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-filled-label"
+                    id="demo-simple-select-filled"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+                {/* </Paper> */}
+              </Grid>
 
-            <Grid item xs={12} sm={6}>
-              {/* <Paper className={classes.paper}> */}
-              <FormControl variant="filled" className={classes.formElementSize}>
-                <InputLabel id="demo-simple-select-filled-label" sm={6}>
-                  No of days available
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-filled-label"
-                  id="demo-simple-select-filled"
+              <Grid item xs={12} sm={6}>
+                {/* <Paper className={classes.paper}> */}
+                <FormControl
+                  variant="filled"
+                  className={classes.formElementSize}
                 >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              {/* </Paper> */}
+                  <InputLabel id="demo-simple-select-filled-label" sm={6}>
+                    No of days available
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-filled-label"
+                    id="demo-simple-select-filled"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+                {/* </Paper> */}
+              </Grid>
+
+              <Grid item xs={12} sm={12}>
+                {/* <Paper className={classes.paper}> */}
+                <formControl className={classes.formElementSize} noValidate>
+                  <TextField
+                    id="date"
+                    label="Which date could you be available"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    className={classes.formElementSize}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </formControl>
+                {/* </Paper> */}
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid item xs={12} sm={12}>
-            {/* <Paper className={classes.paper}> */}
-            <formControl className={classes.formElementSize} noValidate>
-              <TextField
-                id="date"
-                label="Which date could you be available"
-                type="date"
-                defaultValue="2017-05-24"
-                className={classes.formElementSize}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </formControl>
-            {/* </Paper> */}
-          </Grid>
+          </Paper>
         </div>
       </form>
     </Container>
