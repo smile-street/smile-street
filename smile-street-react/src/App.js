e248c159c9413954eb91f256c6b099e20830662e
Ariel Pillemer <pillemer@gmail.com>
2021-02-23 12:28:33 +0000

    sorting out all the imports

--- smile-street-react/src/App.js
+++ smile-street-react/src/App.js
@@ -1,31 +1,28 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Login from "./components/Login/Login";
-import VolunteerMatches from "./components/VolunteerMatches/VolunteerMatches";
-import PasswordRecovery from "./components/PasswordRecovery/PasswordRecovery";
-import Registration from "./components/Registration/Registration";
-import VolunteerInterests from "./components/VolunteerInterests/VolunteerInterests";
-import GoodCauseOpportunity from "./components/GoodCauseOpportunity/GoodCauseOpportunity";
-import GoodCauseDetails from "./components/GoodCauseDetails/GoodCauseDetails";
-import VolunteerAvaiblity from "./components/VolunteerAvaiblity/VolunteerAvaiblity.js";
+import Header from './components/Header/Header';
+import GoodCauseDetails from './components/GoodCauseDetails/GoodCauseDetails';
+import GoodCauseOpportunity from './components/GoodCauseOpportunity/GoodCauseOpportunity';
+import Login from './components/Login/Login';
+import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery';
+import Registration from './components/Registration/Registration';
+import VolunteerAvaiblity from './components/VolunteerAvaiblity/VolunteerAvaiblity';
+import VolunteerInterests from './components/VolunteerInterests/VolunteerInterests';
+import VolunteerMatches from './components/VolunteerMatches/VolunteerMatches';
+
 
 
 function App() {
   return (
     <div className="App">
-
       <Header />
-      <Login />
-      {/* <GoodCauseOpportunity />
       <GoodCauseDetails />
+      <GoodCauseOpportunity />
+      <Login />
+      <PasswordRecovery />
+      <Registration />
+      <VolunteerAvaiblity />
       <VolunteerInterests />
-      <VolunteermatchesYesNoButton />
-      <PasswordRecovery /> */}
       <VolunteerMatches />
-
-
-
-
     </div>
   );
 }
--- smile-street-react/src/components/Header/Header.js
+++ smile-street-react/src/components/Header/Header.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
