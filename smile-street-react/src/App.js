47032ea16faa714a422745d23604cc97d3f6002b
Ariel Pillemer <pillemer@gmail.com>
2021-02-23 12:12:39 +0000

    sorted out imports

--- smile-street-react/src/App.js
+++ smile-street-react/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
-
-// import Volunteermatches from "./components/Volunteermatches/Volunteermatches";
+import VolunteerMatches from "./components/VolunteerMatches/VolunteerMatches";
 import PasswordRecovery from "./components/PasswordRecovery/PasswordRecovery";
 import Registration from "./components/Registration/Registration";
-
 import VolunteerInterests from "./components/VolunteerInterests/VolunteerInterests";
 import GoodCauseOpportunity from "./components/GoodCauseOpportunity/GoodCauseOpportunity";
 import GoodCauseDetails from "./components/GoodCauseDetails/GoodCauseDetails";
@@ -19,12 +17,11 @@ function App() {
       <Header />
       <Login />
       {/* <GoodCauseOpportunity />
-
       <GoodCauseDetails />
       <VolunteerInterests />
       <VolunteermatchesYesNoButton />
       <PasswordRecovery /> */}
-      <Registration />
+      <VolunteerMatches />
 
 
 
