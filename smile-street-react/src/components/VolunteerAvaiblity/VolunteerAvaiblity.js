d5489a5e60e409a6f3484c094c8904fea09b64b8
taslima26 <banglawalas@hotmail.co.uk>
2021-02-24 18:58:24 +0000

    volunteer-avaiblity-form-is-aligned

--- smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvaiblity.js
+++ smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvaiblity.js
@@ -12,6 +12,12 @@ import {
   Select,
 } from "@material-ui/core";
 import PageHeading from "../PageHeading/PageHeading";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -113,10 +119,34 @@ export default function VolunteerAvaiblity() {
                 <MenuItem value={30}>Thirty</MenuItem>
               </Select>
             </FormControl>
+            <InputLabel
+              id="demo-simple-select-outlined-label"
+              style={{ margin: 8 }}
+            >
+              Which dates could you be available
+            </InputLabel>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid container justify="space-around">
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-inline"
+                  label="Date picker inline"
+                  value=""
+                  onChange=""
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                  fullWidth
+                />
+              </Grid>
+            </MuiPickersUtilsProvider>
 
             <Grid item xs={12} sm={12}>
               <Button variant="contained" className={classes.buttonColor}>
-                Add availblity
+                Add Intrests
               </Button>
             </Grid>
           </Grid>
@@ -125,144 +155,3 @@ export default function VolunteerAvaiblity() {
     </Container>
   );
 }
-
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import {
-//   Select,
-//   TextField,
-//   FormControl,
-//   MenuItem,
-//   Container,
-//   Grid,
-//   Paper,
-//   InputLabel,
-//   Button,
-// } from "@material-ui/core";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     textAlign: "center",
-//     color: theme.palette.text.secondary,
-//   },
-//   formElementSize: {
-//     minWidth: "90%",
-//   },
-//   buttonColor: {
-//     backgroundColor: "#53bd98",
-//     color: "white",
-//     "&:hover": {
-//       background: "#449f80",
-//     },
-//   },
-// }));
-
-// const VolunteerAvaiblity = () => {
-//   const classes = useStyles();
-
-//   return (
-//     <Container>
-//       <form>
-//         <div className={classes.root}>
-//           <Paper className={classes.paper}>
-//             <Grid container spacing={3}>
-//               <Grid item xs={12} sm={6}>
-//                 <FormControl
-//                   variant="filled"
-//                   className={classes.formElementSize}
-//                 >
-//                   <InputLabel id="demo-simple-select-filled-label" sm={6}>
-//                     Select your primary match Location
-//                   </InputLabel>
-//                   <Select
-//                     labelId="demo-simple-select-filled-label"
-//                     id="demo-simple-select-filled"
-//                   >
-//                     <MenuItem value="">
-//                       <em>None</em>
-//                     </MenuItem>
-//                     <MenuItem value={10}>Ten</MenuItem>
-//                     <MenuItem value={20}>Twenty</MenuItem>
-//                     <MenuItem value={30}>Thirty</MenuItem>
-//                   </Select>
-//                 </FormControl>
-//               </Grid>
-
-//               <Grid item xs={12} sm={6}>
-//                 <FormControl
-//                   variant="filled"
-//                   className={classes.formElementSize}
-//                 >
-//                   <InputLabel id="demo-simple-select-filled-label" sm={6}>
-//                     Select your secondary match Location
-//                   </InputLabel>
-//                   <Select
-//                     labelId="demo-simple-select-filled-label"
-//                     id="demo-simple-select-filled"
-//                   >
-//                     <MenuItem value="">
-//                       <em>None</em>
-//                     </MenuItem>
-//                     <MenuItem value={10}>Ten</MenuItem>
-//                     <MenuItem value={20}>Twenty</MenuItem>
-//                     <MenuItem value={30}>Thirty</MenuItem>
-//                   </Select>
-//                 </FormControl>
-//               </Grid>
-
-//               <Grid item xs={12} sm={6}>
-//                 <FormControl
-//                   variant="filled"
-//                   className={classes.formElementSize}
-//                 >
-//                   <InputLabel id="demo-simple-select-filled-label" sm={6}>
-//                     No of days available
-//                   </InputLabel>
-//                   <Select
-//                     labelId="demo-simple-select-filled-label"
-//                     id="demo-simple-select-filled"
-//                   >
-//                     <MenuItem value="">
-//                       <em>None</em>
-//                     </MenuItem>
-//                     <MenuItem value={10}>Ten</MenuItem>
-//                     <MenuItem value={20}>Twenty</MenuItem>
-//                     <MenuItem value={30}>Thirty</MenuItem>
-//                   </Select>
-//                 </FormControl>
-//               </Grid>
-
-//               <Grid item xs={12} sm={6}>
-//                 <formControl className={classes.formElementSize} noValidate>
-//                   <TextField
-//                     id="date"
-//                     label="Which date could you be available"
-//                     type="date"
-//                     defaultValue="2017-05-24"
-//                     className={classes.formElementSize}
-//                     InputLabelProps={{
-//                       shrink: true,
-//                     }}
-//                   />
-//                 </formControl>
-//               </Grid>
-//               <Grid item xs={12} sm={12}>
-//                 <formControl className={classes.formElementSize} noValidate>
-//                   <Button variant="contained" className={classes.buttonColor}>
-//                     Add Intrests
-//                   </Button>
-//                 </formControl>
-//               </Grid>
-//             </Grid>
-//           </Paper>
-//         </div>
-//       </form>
-//     </Container>
-//   );
-// };
-
-// export default VolunteerAvaiblity;
