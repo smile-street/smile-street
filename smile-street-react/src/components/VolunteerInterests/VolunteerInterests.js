96f61ced0d345e3689a7e883a138d27916bfced6
Ariel Pillemer <pillemer@gmail.com>
2021-02-28 11:58:23 +0000

    move interest list into useState const

--- smile-street-react/src/components/VolunteerInterests/VolunteerInterests.js
+++ smile-street-react/src/components/VolunteerInterests/VolunteerInterests.js
@@ -27,14 +27,29 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FullWidthGrid() {
   const classes = useStyles();
-  const [selectedInterests, setSelectedInterest] = useState(""); 
+  const [interests, setSelectedInterest] = useState(
+    [
+      { skill: "Conservation", image: "./images/interestImages/conservation.svg", selected: false },
+      { skill: "Teaching", image: "./images/interestImages/teaching.svg", selected: false },
+      { skill: "Public Health", image:  "./images/interestImages/public_health.svg", selected: false },
+      { skill: "Empowerment", image: "./images/interestImages/empowerment.svg", selected: false },
+      { skill: "Sports", image:  "./images/interestImages/sports.svg", selected: false },
+      { skill: "Construction", image:  "./images/interestImages/construction.svg", selected: false },
+      { skill: "Cooking", image:  "./images/interestImages/cooking.svg", selected: false },
+      { skill: "Accessibility", image:  "./images/interestImages/accessibility.svg", selected: false },
+      { skill: "Mental Health", image:  "./images/interestImages/mental_health.svg", selected: false },
+      { skill: "Event Planning", image:  "./images/interestImages/event_planning.svg", selected: false },
+      { skill: "Gardening", image:  "./images/interestImages/gardening.svg", selected: false },
+      { skill: "Music", image:  "./images/interestImages/music.svg", selected: false },
+    ]
+  ); 
 
   return (
       <Container component="main">
         <Paper className={classes.paper}>
           <PageHeading heading="Select your interests" />
           <Grid container spacing={3}>
-              {interestList.map((interest) => {
+              {interests.map((interest) => {
                 return (
                 <Grid item xs={6} sm={3}>
                   <InterestSquares key={interest.skill} title={interest.skill} image={interest.image} />
@@ -55,19 +70,7 @@ export default function FullWidthGrid() {
 }
 
 
-const interestList = [
-  { skill: "Conservation", image: "./images/interestImages/accessibility.svg" },
-  { skill: "Teaching", image: "./images/interestImages/teaching.svg" },
-  { skill: "Public Health", image:  "./images/interestImages/public_health.svg"  },
-  { skill: "Empowerment", image: "./images/interestImages/empowerment.svg" },
-  { skill: "Sports", image:  "./images/interestImages/sports.svg"  },
-  { skill: "Construction", image:  "./images/interestImages/construction.svg"  },
-  { skill: "Cooking", image:  "./images/interestImages/cooking.svg"  },
-  { skill: "Accessibility", image:  "./images/interestImages/accessibility.svg"  },
-  { skill: "Mental Health", image:  "./images/interestImages/mental_health.svg"  },
-  { skill: "Event Planning", image:  "./images/interestImages/event_planning.svg"  },
-  { skill: "Gardening", image:  "./images/interestImages/gardening.svg"  },
-  { skill: "Music", image:  "./images/interestImages/music.svg"  },
+
 
   // <div>Icons made by <a href="https://www.flaticon.com/authors/mangsaabguru" title="mangsaabguru">mangsaabguru</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
@@ -78,4 +81,3 @@ const interestList = [
 
 
 
-];
