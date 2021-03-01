84e2f149b0bd65513fe80dcfa15e216ba2ce03f1
taslima26 <banglawalas@hotmail.co.uk>
2021-03-01 11:45:52 +0000

    removed your app

--- smile-street-react/src/components/Registration/ValidateInfo.js
+++ smile-street-react/src/components/Registration/ValidateInfo.js
@@ -1,45 +1,40 @@
 export default function validateInfo(values) {
-    let errors = {};
-  
-    if (!values.title.trim()) {
-      errors.title = 'Title required';
-    }
-    // else if (!/^[A-Za-z]+/.test(values.name.trim())) {
-    //   errors.name = 'Enter a valid name';
-    // }
+  let errors = {};
 
-    if (!values.firstName.trim()) {
-        errors.firstName = 'First Name required';
-      }
-
-      if (!values.lastName.trim()) {
-        errors.lastName = 'Last Name required';
-      }
-
-    if (!values.email) {
-      errors.email = 'Email required';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = 'Email address is invalid';
-    }
-
-
-    if (!values.contactNumber) {
-        errors.contactNumber = 'Contact Number required';
-      } else if (!/^[0-9]+$/.test(values.contactNumber)) {
-        errors.contactNumber = 'Numeric Characters Only';
-      }
-
-
-    if (!values.password) {
-      errors.password = 'Password is required';
-    } else if (values.password.length < 6) {
-      errors.password = 'Password needs to be 6 characters or more';
-    }
-  
-    if (!values.confirmPass) {
-      errors.confirmPass = 'Password is required';
-    } else if (values.confirmPass !== values.password) {
-      errors.confirmPass = 'Passwords do not match';
-    }
-    return errors;
+  if (!values.title.trim()) {
+    errors.title = "Title required";
   }
+
+  if (!values.firstName.trim()) {
+    errors.firstName = "First Name required";
+  }
+
+  if (!values.lastName.trim()) {
+    errors.lastName = "Last Name required";
+  }
+
+  if (!values.email) {
+    errors.email = "Email required";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Email address is invalid";
+  }
+
+  if (!values.contactNumber) {
+    errors.contactNumber = "Contact Number required";
+  } else if (!/^[0-9]+$/.test(values.contactNumber)) {
+    errors.contactNumber = "Numeric Characters Only";
+  }
+
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 6) {
+    errors.password = "Password needs to be 6 characters or more";
+  }
+
+  if (!values.confirmPass) {
+    errors.confirmPass = "Password is required";
+  } else if (values.confirmPass !== values.password) {
+    errors.confirmPass = "Passwords do not match";
+  }
+  return errors;
+}
