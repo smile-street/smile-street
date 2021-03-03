494655dd5ccbd1291fc29e6ff095a58eeab45828
Ariel Pillemer <pillemer@gmail.com>
2021-03-03 19:47:48 +0000

    create GoodCauseName.js with API call inside. commit before installing dependencies

--- smile-street-react/src/App.js
+++ smile-street-react/src/App.js
@@ -1,34 +1,42 @@
-import './App.css';
-// import Header from "./components/Header/Header";
-// import GoodCauseDetails from "./components/GoodCauseDetails/GoodCauseDetails";
-// import GoodCauseMatches from "./components/GoodCauseMatches/GoodCauseMatches";
-// import Login from "./components/Login/Login";
-// import PasswordRecovery from "./components/PasswordRecovery/PasswordRecovery";
-import VolunteerAvaiblity from './components/VolunteerAvaiblity/VolunteerAvaiblity';
-// import VolunteerInterests from "./components/VolunteerInterests/VolunteerInterests";
+//_______________________________________________________
+//npm install apollo-boost, @apollo/react-hooks, graphql
+import ApolloClient, { gql } from "apollo-boost"
+import { ApolloProvider, useQuery } from "@apollo/react-hooks"
 
-import GoodCauseOpporunity from './components/GoodCauseOpportunity/GoodCauseOpportunity';
-// import Registration from "./components/Registration/Registration";
-import VoluenteerMatches from './components/VolunteerMatches/VoluenteerMatches';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  useHistory,
-} from 'react-router-dom';
-import GoodCauseOpportunity from './components/GoodCauseOpportunity/GoodCauseOpportunity';
+const charityNumber = 1017504 // should be changed by new input from user (e.g 1050488, 1114004)
 
-export default function App() {
-  return (
-    <div className="App">
-      <Header />
-      <VolunteerInterests />
-      {/* <Router>
-        <Header />
-        <Route path="/" exact component={Login} />
-        <Route path="/Registration" exact component={Registration} />
-        <Route path="/PasswordRecovery" exact component={PasswordRecovery} />
-      </Router> */}
-  </div>
-  );
+const client = new ApolloClient({
+  uri: "https://charitybase.uk/api/graphql",
+  headers: {
+    Authorization: "Apikey 93ad424e-fcfd-4d5e-8820-32ecf2291c54",
+  },
+})
+const COUNT_QUERY = gql`
+  {
+    CHC {
+      getCharities(filters: { id: ${charityNumber} }) {
+        list {
+          names { value } 
+        }  
+      }
+    }
+  }
+  
+`
+const CharityName = () => {
+  const { loading, error, data } = useQuery(COUNT_QUERY)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+  return <h1>{data.CHC.getCharities.list[0].names[0].value}</h1>
 }
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <CharityName />
+    </ApolloProvider>
+  )
+}
+
+export default App;
+//_______________________________________________________
---
+++ smile-street-react/src/components/GoodCauseDetails/GoodCauseName.js
@@ -0,0 +1,40 @@
+//npm install apollo-boost, @apollo/react-hooks, graphql
+import ApolloClient, { gql } from "apollo-boost"
+import { ApolloProvider, useQuery } from "@apollo/react-hooks"
+
+const charityNumber = 1017504 // should be changed by new input from user (e.g 1050488, 1114004)
+
+const client = new ApolloClient({
+    uri: "https://charitybase.uk/api/graphql",
+    headers: {
+        Authorization: "Apikey 93ad424e-fcfd-4d5e-8820-32ecf2291c54",
+    },
+})
+const COUNT_QUERY = gql`
+  {
+    CHC {
+      getCharities(filters: { id: ${charityNumber} }) {
+        list {
+          names { value } 
+        }  
+      }
+    }
+  }
+  
+`
+const GetGoodCauseName = () => {
+    const { loading, error, data } = useQuery(COUNT_QUERY)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+    return <h1>{data.CHC.getCharities.list[0].names[0].value}</h1>
+}
+
+const GoodCauseName = () => {
+    return (
+        <ApolloProvider client={client}>
+            <GetGoodCauseName />
+        </ApolloProvider>
+    )
+}
+
+export default GoodCauseName;
