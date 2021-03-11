10ad9d3769c4a330016d11a7359007df1e4e9c97
taslima26 <banglawalas@hotmail.co.uk>
2021-03-11 15:20:25 +0000

    aded footer

--- smile-street-react/src/components/Header/Header.js
+++ smile-street-react/src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Paper, Grid } from "@material-ui/core";
+import React from 'react';
+import {Container, Paper, Grid} from '@material-ui/core';
 
 const Header = () => {
   return (
