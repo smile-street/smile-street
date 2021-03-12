8068b75df0f78fa7c994d0adbb1131ea31c7ea6f
taslima26 <banglawalas@hotmail.co.uk>
2021-03-12 10:05:10 +0000

    styles code is dry

--- smile-street-react/src/components/PageHeading/PageHeading.js
+++ smile-street-react/src/components/PageHeading/PageHeading.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Typography, Paper} from '@material-ui/core';
+import {Typography, Paper, Grid, Container} from '@material-ui/core';
 import useStyle from '../Style/Style';
 
 export default function PageHeading(props) {
   const classes = useStyle();
   return (
-    <Typography component="h1" variant="h5" gutterBottom className={classes.h1}>
-      {props.heading}
-    </Typography>
+    <Container className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h5"
+        gutterBottom
+        className={classes.h1}
+      >
+        {props.heading}
+      </Typography>
+    </Container>
   );
 }
