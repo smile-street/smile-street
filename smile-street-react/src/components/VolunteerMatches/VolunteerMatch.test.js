d51a94aa2c65c298d13a787409911cc1444262fc
taslima26 <banglawalas@hotmail.co.uk>
2021-03-19 10:17:36 +0000

    added first test which is failing when it should be passing

--- smile-street-react/src/components/VolunteerMatches/VolunteerMatch.test.js
+++ smile-street-react/src/components/VolunteerMatches/VolunteerMatch.test.js
@@ -1,104 +1,29 @@
 /* eslint-disable jest/valid-title */
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MatchCard from './MatchCard';
 import VolunteerMatches from './VolunteerMatches';
 
 describe('Volunteer Match  component', () => {
-    afterEach(() => jest.clearAllMocks())
+  afterEach(() => jest.clearAllMocks());
 
-    test(`
+  test(`
         Given the required props,
         When the component is rendered,
-        Then the ta
+        Then the 
          text should be present`, () => {
-            const requiredProps ={
-                complete: false, 
-                text: "This is a task", 
-                id:"001", 
-                deleteTask: () => {}
-            }
+    const requiredProps = {
+      id: '001',
+      GoodCause: 'This  is a Good Cause',
+      Description: 'This is a testing Good Cause',
+      Dates: '26/05/1986',
+      text: 'This is a task',
+      handleAgree: () => {},
+      handleAccepted: () => {},
+    };
 
-            render(<Task { ...requiredProps } />)
+    render(<MatchCard {...requiredProps} />);
 
-            expect(screen.getByText("This is a task")).toBeInTheDocument()
-        })
-
-    test(`
-        Given the required props,
-        When the component is rendered,
-        Then the delete button should be present`, () => {
-            const requiredProps ={
-                complete: false, 
-                text: "This is a task", 
-                id:"001", 
-                deleteTask: () => {}
-            }
-
-            render(<Task { ...requiredProps } />)
-
-            expect(screen
-                .getAllByRole("button")
-                .filter(button => button.textContent === "Delete").length)
-                .toBe(1)
-        })
-
-    test(`
-        Given a completed task,
-        When the component is rendered,
-        Then the complete button should not be present`, () => {
-            const completedTaskProps ={
-                complete: true, 
-                text: "This is a task", 
-                id:"001", 
-                deleteTask: () => {}
-            }
-
-            render(<Task { ...completedTaskProps } />)
-
-            expect(screen
-                .getAllByRole("button")
-                .filter(button => button.textContent === "Complete").length)
-                .toBe(0)
-        })
-
-    test(`
-        Given an incomplete task,
-        When the component is rendered,
-        Then the complete button should be present`, () => {
-            const incompleteTaskProps ={
-                complete: false, 
-                text: "This is a task", 
-                id:"001", 
-                deleteTask: () => {}
-            }
-
-            render(<Task { ...incompleteTaskProps } />)
-
-            expect(screen
-                .getAllByRole("button")
-                .filter(button => button.textContent === "Complete").length)
-                .toBe(1)
-        })
-
-    test(`
-        Given a task is rendered,
-        When the delete button is clicked,
-        Then the delete task function is called with the correct task id`, () => {
-            const mockDeleteFunction = jest.fn()
-            const testTaskId = "001"
-
-            const requiredProps ={
-                complete: false, 
-                text: "This is a task", 
-                id:testTaskId, 
-                deleteTask: mockDeleteFunction
-            }
-
-            render(<Task { ...requiredProps } />)
-
-            const deleteButton = screen.getAllByRole("button").find(button => button.textContent === "Delete")
-            userEvent.click(deleteButton)
-
-            expect(mockDeleteFunction).toHaveBeenCalledWith(testTaskId)
-        })
-}) 
+    expect(screen.getByText('This is a GoodCause')).toBeInTheDocument();
+  });
+});
