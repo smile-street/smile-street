25b407e846b19d843f95b9368f47797662da2294
Ariel Pillemer <pillemer@gmail.com>
2021-03-19 13:41:30 +0000

    generate location menuitems from locations.json

--- smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvailability.js
+++ smile-street-react/src/components/VolunteerAvaiblity/VolunteerAvailability.js
@@ -10,6 +10,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  FormControl,
 } from '@material-ui/core';
 import PageHeading from '../PageHeading/PageHeading';
 import Validation from './Validation';
@@ -29,9 +30,11 @@ export default function VolunteerAvailability() {
   });
 
   const handleChange = (e) => {
+    console.log(e.target.name)
+    console.log(e.target.value)
     setInfo({...info, [e.target.name]: e.target.value,});
   };
-  
+
   const handleSubmit = (e) => {
     setErrors(Validation(info));
     e.preventDefault();
@@ -71,97 +74,42 @@ export default function VolunteerAvailability() {
               {errors.employer_name}
             </Grid>
             <Grid item xs={12} sm={12}>
-              <InputLabel>Select your location</InputLabel>
-              <Select
-                label
-                fullWidth
-                id=""
-                style={{margin: 8}}
-                autoFocus
-                variant="outlined"
-                className={classes.root}
-                name="location"
-                value={info.location}
-                onChange={handleChange}
-              >
-                <MenuItem>
-                  <em>primary match location</em>
-                </MenuItem>
-
-                <MenuItem value={'Bath'}>Bath</MenuItem>
-                <MenuItem value={'Birmingham'}>Birmingham</MenuItem>
-                <MenuItem value={'Bradford'}>Bradford</MenuItem>
-                <MenuItem value={'Brighton'}>Brighton & Hove</MenuItem>
-                <MenuItem value={'Bristol'}>Bristol</MenuItem>
-                <MenuItem value={'Cambridge'}>Cambridge</MenuItem>
-                <MenuItem value={'Canterbury'}>Canterbury</MenuItem>
-                <MenuItem value={'Carlise'}>Carlisle</MenuItem>
-                <MenuItem value={'Chemlsford'}>Chelmsford</MenuItem>
-                <MenuItem value={'Chester'}>Chester</MenuItem>
-                <MenuItem value={'Chichester'}>Chichester</MenuItem>
-                <MenuItem value={'Coventry'}>Coventry</MenuItem>
-                <MenuItem value={'Derby'}>Derby</MenuItem>
-                <MenuItem value={'Durham'}>Durham</MenuItem>
-                <MenuItem value={'Exeter'}>Exeter</MenuItem>
-                <MenuItem value={'Gloucester'}>Gloucester</MenuItem>
-                <MenuItem value={'Hereford'}>Hereford</MenuItem>
-                <MenuItem value={'Kingston'}>Kingston</MenuItem>
-                <MenuItem value={'Lancaster'}>Lancaster</MenuItem>
-                <MenuItem value={'Leeds'}>Leeds</MenuItem>
-                <MenuItem value={'Leicester'}>Leicester</MenuItem>
-                <MenuItem value={'Lichfield'}>Lichfield</MenuItem>
-                <MenuItem value={'Lincoln'}>Lincoln</MenuItem>
-                <MenuItem value={'Liverpool'}>Liverpool</MenuItem>
-                <MenuItem value={'City_of_London'}>(City of) London</MenuItem>
-                <MenuItem value={'Manchester'}>Manchester</MenuItem>
-                <MenuItem value={'Newcastle'}>Newcastle</MenuItem>
-                <MenuItem value={'Norwich'}>Norwich</MenuItem>
-                <MenuItem value={'Nottingham'}>Nottingham</MenuItem>
-                <MenuItem value={'Oxford'}>Oxford</MenuItem>
-                <MenuItem value={'Thirty'}>Thirty</MenuItem>
-                <MenuItem value={'Peterborough'}>Peterborough</MenuItem>
-                <MenuItem value={'Plymouth'}>Plymouth</MenuItem>
-                <MenuItem value={'Portsmouth'}>Portsmouth</MenuItem>
-                <MenuItem value={'Preston'}>Preston</MenuItem>
-                <MenuItem value={'Rippon'}>Rippon</MenuItem>
-                <MenuItem value={'Salford'}>Salford</MenuItem>
-                <MenuItem value={'Salisbury'}>Salisbury</MenuItem>
-                <MenuItem value={'Sheffield'}>Sheffield</MenuItem>
-                <MenuItem value={'Southampton'}>Southampton</MenuItem>
-                <MenuItem value={'St_Albans'}>St Albans</MenuItem>
-                <MenuItem value={'Stoke-On-Trent'}>Stoke-on-Trent</MenuItem>
-                <MenuItem value={'Sunderland'}>Sunderland</MenuItem>
-                <MenuItem value={'Truro'}>Truro</MenuItem>
-                <MenuItem value={'Wakefield'}>Wakefield</MenuItem>
-                <MenuItem value={'Well'}>Wells</MenuItem>
-                <MenuItem value={'City_of_Westminster'}>
-                  (City of) Westminster
-                </MenuItem>
-                <MenuItem value={'Winchester'}>Winchester</MenuItem>
-                <MenuItem value={'Wolverhampton'}>Wolverhampton</MenuItem>
-                <MenuItem value={'Worcester'}>Worcester</MenuItem>
-                <MenuItem value={'York'}>York</MenuItem>
-              </Select>
+              <FormControl variant="outlined" fullWidth>  
+                <InputLabel id="location-label">Select your location</InputLabel>
+                <Select
+                  labelId="location-label"
+                  label="Select your location"
+                  
+                  id="location"
+                  name='location'
+                  style={{margin: 8}}
+                  className={classes.root}
+                  value={info.location}
+                  onChange={handleChange}
+                >
+                  {locations.map(location => {
+                    return(
+                      <MenuItem value={location.name}>{location.name}</MenuItem>
+                    )
+                  })}
+                </Select>
+              </FormControl>
             </Grid>
             <Grid item xs={12} sm={12}>
-              <InputLabel>Please select no. of days available</InputLabel>
-
+              <InputLabel>
+                How many days are you available?
+              </InputLabel>
               <Select
-                label
+                label="Available days"
                 fullWidth
-                id=""
+                id="numberOfDays"
                 style={{margin: 8}}
-                autoFocus
                 variant="outlined"
                 className={classes.root}
                 name="numberOfDays"
                 onChange={handleChange}
                 value={info.numberOfDays}
               >
-                <MenuItem>
-                  <em>No of days available</em>
-                </MenuItem>
-
                 <MenuItem value={'1'}>1</MenuItem>
                 <MenuItem value={'2'}>2</MenuItem>
                 <MenuItem value={'3'}>3</MenuItem>
@@ -173,14 +121,11 @@ export default function VolunteerAvailability() {
                 <MenuItem value={'9'}>9</MenuItem>
                 <MenuItem value={'10'}>10</MenuItem>
               </Select>
-
-              <InputLabel>
-                Between which dates would you like to use your available days/
-                see volunteering opportunities
-              </InputLabel>
             </Grid>
-
             <Grid item xs={12} sm={12}>
+              <InputLabel>
+                Select your available dates
+              </InputLabel>
               <TextField
                 margin="normal"
                 fullWidth
